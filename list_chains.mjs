0ac7b2f1e61f60c0bdcf909f8da9b0470367aebf
ayberk <esena@mef.edu.tr>
2025-11-21 17:18:35 +0300

    Update README to match project

--- list_chains.mjs
+++
@@ -1,9 +0,0 @@
-
-import { chains } from './node_modules/@circle-fin/bridge-kit/index.mjs';
-
-console.log("Available Testnet Chains:");
-Object.values(chains).forEach(chain => {
-    if (chain.isTestnet) {
-        console.log(`- ${chain.name} (Key: ${chain.chain})`);
-    }
-});
